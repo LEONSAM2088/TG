8080d8de5ae6e81275d8170ca057f06e9553102d
LEONSAM2088 <leonsams@mail.ru>
2022-06-09 18:12:46 +0300

    master

--- data/admins.py
+++ data/admins.py
@@ -10,6 +10,10 @@ class Admin:
 
 Admins = [Admin(
     id=0,
-    card="0000 0000 1111 2222",
+    card="5536 9140 3901 8001",
+    tg_id=1836632238
+), Admin(
+    id=1,
+    card="",
     tg_id=1066208089
 )]
